73754114f106ee2b7f1d921b29038447f6a96aab
WilianCarvalho <wilian-carvalho@hotmail.com>
2022-03-09 23:24:36 -0300

    09/03 Migration

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import RegistroCliente
+from .models import RegistroContato
+
+admin.site.register(RegistroCliente)
+admin.site.register(RegistroContato)
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+#Classe Registro de Cliente
+class RegistroCliente(models.Model):
+
+    NumCnpj = models.CharField(max_length=14)
+    RazSoci = models.CharField(max_length=255)
+    NomFant = models.CharField(max_length=255)
+    DataGer = models.DateTimeField(auto_now_add=True)
+    DataAut = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.RazSoci
+
+#Classe Registro de Contato
+class RegistroContato(models.Model):
+
+    TipoContato = (
+        ('TEL', 'Telefone'),
+        ('WAP', 'WhatsApp'),
+        ('EML', 'E-mail'),
+    )
+
+    Situacao = (
+        ('PRP','Prospecção'),
+        ('ART','Aguardando Retorno'),
+        ('PEF','Pedido Efetivado'),
+        ('CAN','Cancelamento'),
+        ('SIT','Sem Interresse'),
+        ('PCA','Pedido Cancelado'),
+        ('COB','Cobrança')
+    )
+
+    NumCnpj = models.CharField(max_length=14)
+    DataCon = models.DateField(auto_now_add=True)
+    HoraCon = models.TimeField(auto_now_add=True)
+    TipoCon = models.CharField(
+        max_length = 3,
+        choices = TipoContato,
+    )
+    SitiCon = models.CharField(
+        max_length = 3,
+        choices = Situacao,
+    ) 
+    ObseCon = models.TextField()
+    
+    def __str__(self):
+        return self.NumCnpj
